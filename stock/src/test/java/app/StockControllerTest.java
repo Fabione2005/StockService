5adc0f243f146c6fc2e125dc41a6656b81651209
Fabione <fabione2005@gmail.com>
2020-06-17 19:47:21 -0400

    Agregando metodos Test de servicios

--- stock/src/test/java/app/StockControllerTest.java
+++ stock/src/test/java/app/StockControllerTest.java
@@ -7,6 +7,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,20 +24,38 @@ public class StockControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 	
-	@Test
+//	@Test
+//	@Order(1)
 	public void retrieveStocks() throws Exception {
 		mockMvc.perform(get("/stocks")).andDo(print());
 	}
 	
-	@Test
+	//@Test
 	public void deleteStock()throws Exception {
 		mockMvc.perform(delete("/stocks/2"));
 	}
 	
+	//@Test
 	public void addStock() throws Exception{
 		mockMvc.perform(post("/stocks")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content("{\"name\":\"SpaceX\",\"lastUpdate\":\"2020-06-17\",\"lastPrice\":120.4,\"actualPrice\":125.7}")
+				);
+	}
+	
+//	@Test
+//	@Order(0)
+	public void updateStock() throws Exception{
+		mockMvc.perform(put("/stocks")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"id\":7,\"name\":\"Toshiba\",\"lastUpdate\":\"2020-06-18\",\"lastPrice\":155.3,\"actualPrice\":115.2}")
+				);
+	}
+	
+	@Test
+	@Order(0)
+	public void retrieveStockByLessDevelopmentUpdate() throws Exception{
+		mockMvc.perform(get("/stocks/developmentUpdateLess/1")
 				).andDo(print());
 	}
 
